8a6d3670afe3095f82d5522d9b799cfe1513c41d
IzTran05 <izabellatran@gmail.com>
2025-06-07 04:55:16 +1000

    Lesson 4.4 Done!
    Power ups that spawn and instantiate, enemy waves also added.

--- Assets/Universal/Scripts/SpawnManager.cs
+++ Assets/Universal/Scripts/SpawnManager.cs
@@ -1,24 +1,40 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerUpPrefab;
 
     void Start()
     {
-        //float spawnPosX = Random.Range(-spawnRange, spawnRange);
-       // float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-
-        //Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
     }
 
 
     void Update()
     {
-        
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
