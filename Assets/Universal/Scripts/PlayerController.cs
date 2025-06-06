6ce3bd6cf5e9d9d0d292789503af5d53ca4ce860
IzTran05 <izabellatran@gmail.com>
2025-06-07 02:43:01 +1000

    Lesson 4.1 Done!
    Added in camera rotation and player movement.

---
+++ Assets/Universal/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    private GameObject focalPoint;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+   
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
