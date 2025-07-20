2b859b1ca5c04becdaa182f32b6c29b9637924b6
IzTran05 <izabellatran@gmail.com>
2025-07-21 02:48:19 +1000

    Adjusted Vine Script to have multiple vine anchors

--- Assets/Prototype-3/Scripts/VineSwing.cs
+++ Assets/Prototype-3/Scripts/VineSwing.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class VineSwing : MonoBehaviour
 {
-    public Transform vineAnchor;
+    public float swingRange = 7f;
+    public LayerMask vineLayer;
     private SpringJoint joint;
     private Rigidbody rb;
 
@@ -16,25 +17,51 @@ public class VineSwing : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (joint == null)
-                AttachToVine();
+                TryAttachToVine();
             else
                 DetachFromVine();
         }
     }
 
-    void AttachToVine()
+
+    void TryAttachToVine()
+    {
+        Collider[] vines = Physics.OverlapSphere(transform.position, swingRange, vineLayer);
+        if (vines.Length > 0)
+        {
+            Transform closest = vines[0].transform;
+            float minDist = Vector3.Distance(transform.position, closest.position);
+
+            foreach (var vine in vines)
+            {
+                float dist = Vector3.Distance(transform.position, vine.transform.position);
+                if (dist < minDist)
+                {
+                    closest = vine.transform;
+                    minDist = dist;
+                }
+            }
+
+            AttachToVine(closest);
+        }
+    }
+
+    void AttachToVine(Transform anchor)
     {
         joint = gameObject.AddComponent<SpringJoint>();
         joint.autoConfigureConnectedAnchor = false;
-        joint.connectedAnchor = vineAnchor.position;
+        joint.connectedAnchor = anchor.position;
 
-        joint.spring = 50f;
+        joint.spring = 100f;
         joint.damper = 5f;
-        joint.maxDistance = 2f;
+        joint.maxDistance = 2.5f;
     }
 
     void DetachFromVine()
     {
         Destroy(joint);
+
+        Vector3 forwardSwing = transform.forward + Vector3.up;
+        rb.AddForce(forwardSwing * 5f, ForceMode.VelocityChange);
     }
 }
