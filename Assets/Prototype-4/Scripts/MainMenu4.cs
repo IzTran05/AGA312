92250c2dd4ddf92802dcaa271f6ddfd1dbda15fa
IzTran05 <izabellatran@gmail.com>
2025-07-29 10:22:30 +1000

    Added in title screen
    title screen with play button and quit button

---
+++ Assets/Prototype-4/Scripts/MainMenu4.cs
@@ -0,0 +1,16 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu4 : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("MathIslandAdventure");
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
